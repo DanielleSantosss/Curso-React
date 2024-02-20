d6acc8e0754b90ae2f0ca4cb41e84037e2fea1af
Danielle Vitória dos Santos <danielle.vtds@gmail.com>
2024-02-19 22:42:00 -0300

    Update App.js

--- projeto-em-react/src/App.js
+++ projeto-em-react/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const name = 'Danielle'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
   return (
     <div className="App">
-      <h1>Olá mundo e React!</h1>
-      <p>Meu primeiro projeto.</p>
+      <h2>Alterando o JSX.</h2>
+      <p>Olá, {newName}.</p>
+      <p>Soma: {sum(1, 5)}</p>
+      <img src={url} alt="Minha imagem." />
     </div>
   );
 }
