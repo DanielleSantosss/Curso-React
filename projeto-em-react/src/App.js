83d3511db87178e797613369be744ff0a8a00cdf
Danielle Vitória dos Santos <danielle.vtds@gmail.com>
2024-02-20 22:01:27 -0300

    Componentes.
    Criando o primeiro componente em React.

--- projeto-em-react/src/App.js
+++ projeto-em-react/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld'
 
 function App() {
   const name = 'Danielle'
@@ -15,6 +16,7 @@ function App() {
       <p>Olá, {newName}.</p>
       <p>Soma: {sum(1, 5)}</p>
       <img src={url} alt="Minha imagem." />
+      <HelloWorld/>
     </div>
   );
 }
