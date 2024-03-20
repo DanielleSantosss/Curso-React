1aee2a7947ccb8d1bdecaa86c917bf9f3f6a90bf
Danielle Vitória dos Santos <danielle.vtds@gmail.com>
2024-03-20 20:12:03 -0300

    Alterações para o Route.

--- projeto-em-react/src/components/Layout.jsx
+++ projeto-em-react/src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
 
 function Layout({children}){
     return(
@@ -8,6 +9,9 @@ function Layout({children}){
                  <li><Link to='/'>Home</Link></li>
                 <li><Link to='/sobreMim'>Sobre Mim</Link></li>
                 <li><Link to='/Contato'>Contato</Link></li>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
              </ul>
              {children}
         </>       
